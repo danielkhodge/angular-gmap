9542fbc36d92811aac1def4ddb7f28c24bf3422c
Daniel Hodge <danielkhodge01@gmail.com>
2020-04-24 14:50:07 -0500

    second commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,45 +1,29 @@
 import { Component, AfterViewInit, ViewChild, ElementRef, OnInit, Input } from '@angular/core';
-import { logging } from 'protractor';
 
 @Component({
   selector: 'app-home',
-  // templateUrl: './home.component.html',
+  templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  template: `
-    <p>From Google Earth, select a location on the map by simply left-mouse clicking. A gray icon will appear at the exact location. Click this icon again and a blue icon will appear with the exact negative and positive of the location.</p>
-
-<div>
-    Input Latitude: <input type="number" ng-bind="lat" value="{{latitude}}"/>
-</div>
-<div>
-    Input Longitude: <input type="number" ng-bind="lng" value="{{longitude}}"/>
-</div>
-<div>
-    <button (click)="mapInitializer()">View Location</button>
-</div>
-
-<div #mapContainer id="map"></div>
-  `
 })
 
 export class HomeComponent {
 
   @ViewChild('mapContainer', {static: false}) gmap: ElementRef;
 
-  @Input() longitude: string;
-  @Input() latitude: string;
+  latitude: number = 41.1889614;
+  longitude: number = -112.0896354;
 
   title = 'angular-gmap';
   map: google.maps.Map;
   pegs: google.maps.Marker[] = [];
 
-  lat = 41.1889614;
-  lng = -112.0896354;
+  // lat = 41.1889614;
+  // lng = -112.0896354;
 
   lat1 = 49.19;
   lng1 = -109.0896354;
 
-  coordinates = new google.maps.LatLng(this.lat, this.lng);
+  coordinates = new google.maps.LatLng(this.latitude, this.longitude);
   coordinates1 = new google.maps.LatLng(this.lat1, this.lng1);
 
   mapOptions: google.maps.MapOptions = {
@@ -55,7 +39,11 @@ export class HomeComponent {
     position: this.coordinates1,
   });
 
-  public addLocation(latitude, longitude) {
+  public addLocation() {
+    this.addPeg(this.latitude, this.longitude);
+  }
+
+  public addPeg(latitude, longitude) {
     var mk = new google.maps.Marker({
       position: new google.maps.LatLng(latitude, longitude),
       map: this.map
